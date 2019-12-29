16ddf1704251025f72dba4d036e7497c4506f51b
Ludwig <lboess@usm.lmu.de>
2019-12-29 21:51:57 +0100

    added COM calculation

---
+++ src/utility/gravity_utility.jl
@@ -0,0 +1,20 @@
+"""
+calculate_center_of_mass(x, m)
+
+    Calculates the center of mass.
+"""
+function calculate_center_of_mass(x, m)
+
+    dt = typeof(m[1])
+    com = Array{dt,1}(undef,3)
+
+    com[1] = sum(m .* x[:,1])
+    com[2] = sum(m .* x[:,2])
+    com[3] = sum(m .* x[:,3])
+
+    mtot = sum(m)
+
+    com ./= mtot
+
+    return com
+end
