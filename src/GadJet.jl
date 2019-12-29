3f67d7a6001713bdec9cc949e13c11cf8c8a067a
Ludwig <lboess@usm.lmu.de>
2019-12-29 21:50:20 +0100

    Changed code structure for snapshot reading  and added wrapper functions for simplification

--- src/GadJet.jl
+++ src/GadJet.jl
@@ -2,18 +2,29 @@
 
 module GadJet
 
+    # general utility stuff
+    include(joinpath(dirname(@__FILE__), "utility", "gravity_utility.jl"))
 
+    # functions to read snapshots
     include(joinpath(dirname(@__FILE__), "read_snapshot", "gadget_types.jl"))
-    include(joinpath(dirname(@__FILE__), "read_snapshot", "dict_functions.jl"))
-    include(joinpath(dirname(@__FILE__), "read_snapshot", "obj_functions.jl"))
+    #include(joinpath(dirname(@__FILE__), "read_snapshot", "dict_functions.jl"))
+    #include(joinpath(dirname(@__FILE__), "read_snapshot", "obj_functions.jl"))
+    include(joinpath(dirname(@__FILE__), "read_snapshot", "read_header.jl"))
+    include(joinpath(dirname(@__FILE__), "read_snapshot", "snapshot_utilities.jl"))
+    include(joinpath(dirname(@__FILE__), "read_snapshot", "read_format_1.jl"))
+    include(joinpath(dirname(@__FILE__), "read_snapshot", "read_format_2.jl"))
+    include(joinpath(dirname(@__FILE__), "read_snapshot", "read_snapshot.jl"))
+    # functions to write snapshots
     include(joinpath(dirname(@__FILE__), "write_snapshot", "write_snap.jl"))
     # sph to grid mapping internal module
     include(joinpath(dirname(@__FILE__), "sph_to_grid", "kernels.jl"))
-    include(joinpath(dirname(@__FILE__), "sph_to_grid", "sph_to_grid.jl"))
     include(joinpath(dirname(@__FILE__), "sph_to_grid", "sph_types.jl"))
+    include(joinpath(dirname(@__FILE__), "sph_to_grid", "mapping_functions.jl"))
+    include(joinpath(dirname(@__FILE__), "sph_to_grid", "sph_to_grid.jl"))
+
     # sph to grid mapping with Smac
     include(joinpath(dirname(@__FILE__), "sph_to_grid", "smac1_utility.jl"))
-    # sph to grid mapping with Smac
+    # sph to grid mapping with P-Smac2
     include(joinpath(dirname(@__FILE__), "sph_to_grid", "smac2_utility.jl"))
     # unit conversion
     include(joinpath(dirname(@__FILE__), "unit_conversion", "unit_types.jl"))
@@ -21,6 +32,8 @@ module GadJet
     #include(joinpath(dirname(@__FILE__), "riemann_solvers", "riemann_solver.jl"))
 
     # test for new riemann solvers
+    include(joinpath(dirname(@__FILE__), "riemann_solvers", "cr_dsa_models.jl"))
+    include(joinpath(dirname(@__FILE__), "riemann_solvers", "cr_sod_shock_main.jl"))
     include(joinpath(dirname(@__FILE__), "riemann_solvers", "setup_riemann_parameters.jl"))
     #include(joinpath(dirname(@__FILE__), "riemann_solvers", "cr_sod_shock.jl"))
 
@@ -34,9 +47,16 @@ module GadJet
            head_to_obj,
            print_blocks,
            read_info,
+           read_snap,
            read_block_by_name,      # similar to readnew.pro by Klaus Dolag
+           read_header,
+
+           # write snapshot functions
            write_header,
            write_block,
+
+           # utility stuff
+           calculate_center_of_mass,
 
            # Kernels
            Cubic,
@@ -47,6 +67,8 @@ module GadJet
            mappingParameters,
            sphAdaptiveMapping,
            sphCenterMapping,
+           glimpse,
+           
            # helper functions and datatypes for Smac
            Smac1ImageInfo,
            read_smac1_binary_image,
@@ -74,233 +96,5 @@ module GadJet
            calculateCRNumber
 
 
-
-
-    # returns an array of present blocknames
-    function print_blocks(filename)
-
-        f = open(filename)
-        blocksize = read(f, Int32)
-
-        if blocksize != 8
-            return "Not possible - use snap_format 2!"
-        end
-
-        p = position(f)
-        seek(f,4)
-
-        blocks = []
-
-        while eof(f) != true
-
-            name = Char.(read!(f, Array{Int8,1}(undef,4)))
-            blockname = String(name)
-
-            blockname = strip(blockname)
-
-            push!(blocks, blockname)
-
-            p = position(f)
-            seek(f,p+8)
-
-            skipsize = read(f, Int32)
-
-            p = position(f)
-            seek(f,p+skipsize+8)
-
-        end
-
-        println("Found blocks: ")
-        for block ∈ blocks
-            println(block)
-        end
-
-        return blocks
-
-    end
-
-    function read_info(filename; verbose::Bool=false)
-
-        f = open(filename)
-        seek(f,4)
-
-        while eof(f) != true
-
-            name = Char.(read!(f, Array{Int8,1}(undef,4)))
-            blockname = String(name)
-
-            p = position(f)
-            seek(f,p+8)
-
-            skipsize = read(f, Int32)
-
-            if blockname == "INFO"
-                n_blocks = Int(skipsize/40)
-                arr_info = Array{Info_Line,1}(undef,n_blocks)
-
-                for i = 1:n_blocks
-                    arr_info[i] = read_info_line(f)
-                end # for
-
-                close(f)
-
-                if verbose == true
-                    println("Found Info block.\nEntries are:\n")
-                    for i ∈ 1:length(arr_info)
-                        println(i, " - ", arr_info[i].block_name)
-                    end # for
-                end # verbose
-
-                return arr_info
-
-            else
-                p = position(f)
-                seek(f,p+skipsize+8)
-            end # if blockname == "INFO"
-
-        end # while eof(f) != true
-
-        println("No info block present!")
-
-        return 1
-
-    end
-
-    function read_info_line(f)
-
-        name = Char.(read!(f, Array{Int8,1}(undef,4)))
-        blockname = String(name)
-
-        block_name = strip(blockname)
-
-        letters = read!(f, Array{Int8,1}(undef,8))
-        letters = Char.(letters)
-        letters = string.(letters)
-
-        data_type = ""
-        for i = 1:length(letters)
-            data_type *= letters[i]
-        end
-
-        data_type = lowercase(strip(data_type))
-
-        if data_type == "float" ||
-           data_type == "floatn"
-                dt = Float32
-        elseif data_type == "long"
-                dt = Int32
-        elseif data_type == "llong"
-                dt = Int64
-        elseif data_type == "double" ||
-               data_type == "doublen"
-                dt = Float64
-        end
-
-        n_dim = read(f,Int32)
-
-        is_present = read!(f, Array{Int32,1}(undef,6))
-
-        in_l = Info_Line(block_name, dt, n_dim, is_present)
-
-        return in_l
-    end
-
-
-    function read_block_by_name(filename::String, blockname::String;
-                                info::Info_Line=Info_Line(),
-                                npart::Vector{Int32}=Int32.(zeros(6)),
-                                parttype::Int64=-1)
-
-
-        if parttype == -1
-            parttypes = ["PartType0", "PartType1", "PartType2",
-                         "PartType3", "PartType4", "PartType5"]
-        end # if parttype
-
-        blockname = strip(blockname)
-
-        if info.block_name == ""
-            info = read_info(filename)
-            if info == 1
-                error("No Info block in snapshot! Supply Info_Line type!")
-                return 1
-            end
-            for i ∈ 1:length(info)
-                if info[i].block_name == blockname
-                    info = info[i]
-                    break
-                end
-            end
-        end
-
-        if npart == Int32.(zeros(6))
-            h = head_to_obj(filename)
-            npart = h.npart
-        end
-
-        f = open(filename)
-
-        first_block = read(f, Int32)
-
-        if first_block != Int32(8)
-            error("Only snapshots of format 2 can be read by name!")
-            return
-        end
-
-        while eof(f) != true
-            name = Char.(read!(f, Array{Int8,1}(undef,4)))
-            blname = String(name)
-            blname = strip(blname)
-
-            if blname == blockname
-                break
-            end
-            p = position(f)
-            seek(f,p+8)
-            skipsize = read(f, Int32)
-            p = position(f)
-            seek(f,p+skipsize+8)
-        end
-
-        if eof(f) != true
-            p = position(f)
-            seek(f,p+8)
-
-            blocksize = read(f,Int32)
-
-            d = Dict()
-
-            for i ∈ 1:length(info.is_present)
-                p = position(f)
-
-                if info.is_present[i] == Int32(1)
-                    if parttype == -1
-                        d[parttypes[i]] = Dict()
-                        d[parttypes[i]][blockname] = copy(transpose(
-                                                            read!(f, Array{info.data_type,2}(undef,(info.n_dim,npart[i])))))
-                    else
-                        if i == (parttype+1)
-
-                            block = copy(transpose(
-                                            read!(f, Array{info.data_type,2}(undef,(info.n_dim,npart[i])))))
-                            close(f)
-                            return block
-                        else
-                            seek(f, p + ( sizeof(info.data_type)*info.n_dim*npart[i] ))
-                        end # if i == (parttype+1)
-                    end # parttype == -1
-                end # info.is_present[i] == Int32(1)
-            end # i ∈ 1:length(info.is_present)
-
-
-            close(f)
-
-            return d
-        else
-
-            return "Block not present!"
-        end # eof(f) != true
-
-    end
 
 end
--- src/read_snapshot/obj_functions.jl
+++ src/read_snapshot/obj_functions.jl
@@ -10,65 +10,6 @@
     Created: 2018-12-12
 
 """
-function head_to_obj(filename)
-
-    h = Header()
-
-    f = open(filename)
-    blocksize = read(f, Int32)
-
-    if blocksize == 8
-        swap = 0
-        snap_format = 2
-    elseif blocksize == 256
-        swap = 0
-        snap_format = 1
-    else
-        blocksize = bswap(blocksize)
-        if blocksize == 8
-            swap = 1
-            snap_format = 2
-        elseif blocksize == 256
-            swap = 1
-            snap_format = 1
-        else
-            println("incorrect file format encountered when reading header of", filename)
-        end
-    end
-
-    #println("Reading snapshot format: ", snap_format)
-
-    if snap_format == 2
-        seek(f, 16)
-        skip_line = read(f, Int32)
-    end
-
-    h.npart = read!(f, Array{Int32,1}(undef,6))
-    h.massarr = read!(f, Array{Float64,1}(undef,6))
-    h.time = read(f, Float64)
-    h.z = read(f, Float64)
-    h.flag_sfr = read(f, Int32)
-    h.flag_feedback = read(f, Int32)
-    h.nall = read!(f, Array{UInt32,1}(undef,6))
-    h.flag_cooling = read(f, Int32)
-    h.num_files = read(f, Int32)
-    h.boxsize = read(f, Float64)
-    h.omega_0 = read(f, Float64)
-    h.omega_l = read(f, Float64)
-    h.h0 = read(f, Float64)
-    h.flag_stellarage = read(f, Int32)
-    h.flag_metals = read(f, Int32)
-    h.npartTotalHighWord = read!(f, Array{UInt32,1}(undef,6))
-    h.flag_entropy_instead_u = read(f, Int32)
-    h.flag_doubleprecision = read(f, Int32)
-    h.flag_ic_info = read(f, Int32)
-    h.lpt_scalingfactor = read(f, Float32)
-
-    close(f)
-
-    return h
-
-end
 
 
 # function snap_to_obj(filename)
