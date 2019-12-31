f1816daefdb43a6a6ee01ce4df28e66363afc3ef
Ludwig <lboess@usm.lmu.de>
2019-12-31 11:41:30 +0100

    added master_doc specification

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,9 @@ release = '0.1.0'
 # ones.
 extensions = ['recommonmark']
 
+
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
