b0ff5442afc6fa9d6cd9307d86e4adb5410ee89e
Ludwig <lboess@usm.lmu.de>
2020-01-02 23:40:33 +0100

    slight changes to prepare for overhaul

--- src/unit_conversion/unit_types.jl
+++ src/unit_conversion/unit_types.jl
@@ -34,9 +34,9 @@ mutable struct GadgetUnitFactors
     P_CR_si::Float64
 
     function GadgetUnitFactors(l_unit=3.085678e21, m_unit=1.989e43, v_unit=1.e5;
-                         z=0., hpar=0.7, γ_th=5.0/3.0, γ_CR=4.0/3.0, xH=0.76)
+                         z=nothing, cosmo_a=nothing, hpar=0.7, γ_th=5.0/3.0, γ_CR=4.0/3.0, xH=0.76)
 
-        t_unit = l_unit/v_unit / hpar
+        t_unit = l_unit/v_unit / hpar  # in sec
         t_Myr = t_unit/3.1536e+13   # in Myr
 
         E_unit = m_unit*v_unit^2
