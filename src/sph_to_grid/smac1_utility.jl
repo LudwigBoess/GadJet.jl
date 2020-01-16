ebe0212c7e02dc65e1c8cb43476372b6f363bc77
LudwigBoess <lboess@usm.lmu.de>
2020-01-16 12:18:54 +0100

    added comments on type fields

--- src/sph_to_grid/smac1_utility.jl
+++ src/sph_to_grid/smac1_utility.jl
@@ -4,21 +4,21 @@
 
 struct Smac1ImageInfo
 
-    snap::Int32
-    z::Float32
-    m_vir::Float32
-    r_vir::Float32
-    xcm::Float32
-    ycm::Float32
-    zcm::Float32
-    z_slice_kpc::Float32
-    boxsize_kpc::Float32
-    boxsize_pix::Float32
-    pixsize_kpc::Float32
-    xlim::Array{Float64,1}
-    ylim::Array{Float64,1}
-    zlim::Array{Float64,1}
-    units::String
+    snap::Int32                 # number of input snapshot
+    z::Float32                  # redshift of snapshot
+    m_vir::Float32              # virial mass of halo
+    r_vir::Float32              # virial radius of halo
+    xcm::Float32                # x coordinate of image center
+    ycm::Float32                # y coordinate of image center
+    zcm::Float32                # z coordinate of image center
+    z_slice_kpc::Float32        # depth of the image in kpc
+    boxsize_kpc::Float32        # xy-size of the image in kpc
+    boxsize_pix::Float32        # xy-size of the image in pixels
+    pixsize_kpc::Float32        # size of one pixel in kpc
+    xlim::Array{Float64,1}      # x limits of image
+    ylim::Array{Float64,1}      # y limits of image
+    zlim::Array{Float64,1}      # z limits of image
+    units::String               # unitstring of image
 
     function Smac1ImageInfo(snap::Int32, z::Float32, m_vir::Float32, r_vir::Float32,
                        xcm::Float32, ycm::Float32, zcm::Float32,
