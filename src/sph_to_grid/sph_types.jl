df7e6ade7f90c34c43122e39f0373cbe4cc72685
Ludwig <lboess@usm.lmu.de>
2020-03-31 16:56:32 +0200

    Changed mappingParameters struct to also work with a given center position

--- src/sph_to_grid/sph_types.jl
+++ src/sph_to_grid/sph_types.jl
@@ -17,36 +17,63 @@
 
 Parameter object for sph to grid mapping. Define either `pixelSideLength` of `Npixels`.
 """
-mutable struct mappingParameters
+struct mappingParameters
 
     x_lim::Vector{Float64}
     y_lim::Vector{Float64}
     z_lim::Vector{Float64}
-    x_max::Vector{Float64}
-    y_max::Vector{Float64}
-    z_max::Vector{Float64}
+    center::Vector{Float64}
     pixelSideLength::Float64
     pixelArea::Float64
     Npixels::Int64
-    xy_size::Float64
+    x_size::Float64
+    y_size::Float64
     z_size::Float64
-    x::Array{Float64,1}
-    y::Array{Float64,1}
-    z::Array{Float64,1}
+    x::Vector{Float64}
+    y::Vector{Float64}
+    z::Vector{Float64}
 
-    function mappingParameters(;x_lim::Vector{Float64},
-                                 y_lim::Vector{Float64},
-                                 z_lim::Vector{Float64},
-                                 pixelSideLength::Float64=0.0,
-                                 Npixels::Int64=0)
+    function mappingParameters(;x_lim::Vector{Float64}   = [-1.0, -1.0],
+                                y_lim::Vector{Float64}   = [-1.0, -1.0],
+                                z_lim::Vector{Float64}   = [-1.0, -1.0],
+                                center::Vector{Float64}  = [-1.0, -1.0, -1.0],
+                                x_size::Float64          =  -1.0,
+                                y_size::Float64          =  -1.0,
+                                z_size::Float64          =  -1.0,
+                                pixelSideLength::Float64 =  -1.0,
+                                Npixels::Int64           =   0)
 
-        xy_size = maximum( [ abs(x_lim[1] - x_lim[2]),
-                             abs(y_lim[1] - y_lim[2]) ] )
 
-        z_size = abs(z_lim[1]) + abs(z_lim[2])
+        # calculate limits if center position and sizes are given
+        if ( x_lim == [-1.0, -1.0] && ( y_lim == [-1.0, -1.0] && z_lim == [-1.0, -1.0] ))
 
-        if (pixelSideLength == 0.0) & (Npixels != 0)
-            pixelSideLength = xy_size/Npixels
+            if (center != [-1.0, -1.0, -1.0]) && ( x_size != -1.0 && (y_size != -1.0 && z_size != -1.0) )
+
+               x_lim = [ center[1] - 0.5x_size, center[1] + 0.5x_size ]
+               y_lim = [ center[2] - 0.5y_size, center[2] + 0.5y_size ]
+               z_lim = [ center[3] - 0.5z_size, center[3] + 0.5z_size ]
+
+            else
+                error("Giving a center position requires extent in x, y and z direction.")
+            end
+        end
+
+        # calculate side lengths from limits
+        if x_size == -1.0
+            x_size = x_lim[2] - x_lim[1]
+        end
+        if y_size == -1.0
+            y_size = y_lim[2] - y_lim[1]
+        end
+        if z_size == -1.0
+            z_size = z_lim[2] - z_lim[1]
+        end
+
+        # find the maximum extent of the map
+        max_size = max(x_size, y_size)
+
+        if (pixelSideLength == -1.0) & (Npixels != 0)
+            pixelSideLength = max_size/Npixels
         elseif (pixelSideLength != 0.0) & (Npixels == 0)
             Npixels = floor(Int64, xy_size/pixelSideLength)
         else
@@ -55,21 +82,17 @@ mutable struct mappingParameters
 
         pixelArea = pixelSideLength^2
 
-        x_max = collect(x_lim[1]:pixelSideLength:x_lim[2])
-        y_max = collect(y_lim[1]:pixelSideLength:y_lim[2])
-        z_max = collect(z_lim[1]:pixelSideLength:z_lim[2])
-
-        x = collect(x_lim[1] : pixelSideLength/2. : x_lim[2])[2:2:end]
-        y = collect(y_lim[1] : pixelSideLength/2. : y_lim[2])[2:2:end]
-        z = collect(z_lim[1] : pixelSideLength/2. : z_lim[2])[2:2:end]
+        x = collect(range(x_lim[1]+0.5pixelSideLength, x_lim[2]-0.5pixelSideLength, step = pixelSideLength))
+        y = collect(range(y_lim[1]+0.5pixelSideLength, y_lim[2]-0.5pixelSideLength, step = pixelSideLength))
+        z = collect(range(z_lim[1]+0.5pixelSideLength, z_lim[2]-0.5pixelSideLength, step = pixelSideLength))
 
 
         new(x_lim, y_lim, z_lim,
-            x_max, y_max, z_max,
+            center,
             pixelSideLength,
             pixelArea,
             Npixels,
-            xy_size, z_size,
+            x_size, y_size, z_size,
             x, y, z)
 
     end
