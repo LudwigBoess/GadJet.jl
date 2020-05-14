5ecd1fdcff936de00a3249dba0dfe0da5e6e35e0
LudwigBoess <lboess@usm.lmu.de>
2020-05-14 13:44:58 +0200

    simplified import

--- src/spectral_cr_utility/get_detailled_data.jl
+++ src/spectral_cr_utility/get_detailled_data.jl
@@ -1,43 +1,34 @@
 import CSV
 
-function get_detailled_crs_data(path::String, filebase::String, mpi_ranks::Int, selected_id::Int)
+function get_detailled_crs_data(path::String, filebase::String, selected_id::Integer)
 
-    read_first = false
+    # select all relevant files
+    files = path .* filter!(s->occursin(r"^" * "$filebase", s), readdir(path))
 
-    @inbounds for mpi_rank = 0:(mpi_ranks-1)
+    # read files
+    df = mapreduce(CSV.read, vcat, files)
 
-        fi = path * filebase * "_$mpi_rank.txt"
+    # delete all empty rows
+    CSV.dropmissing!(df)
 
-        d = CSV.read(fi)
+    # filter dataframe by relevant id
+    df = df[df.ID .== selected_id,:]
 
-        df2 = d[d.ID .== selected_id,:]
-
-        if length(df2[!, 1]) > 0
-            if (read_first == false)
-                global df = df2
-                read_first = true
-            else
-                append!(df, df2)
-            end
-        end
-
-    end
-
-    return sort!(df)
+    return sort!(df[df[:, :ID] .== selected_id,:])
 end
 
-function get_detailled_shock_data(path::String, mpi_ranks::Int, selected_id::Int)
+function get_detailled_shock_data(path::String, selected_id::Integer)
     return get_detailled_crs_data(path, "CRs_details_shock_injection", mpi_ranks, selected_id)
 end
 
-function get_detailled_Dpp_data(path::String, mpi_ranks::Int, selected_id::Int)
+function get_detailled_Dpp_data(path::String, selected_id::Integer)
     return get_detailled_crs_data(path, "CRs_details_Dpp", mpi_ranks, selected_id)
 end
 
-function get_detailled_radiative_data(path::String, mpi_ranks::Int, selected_id::Int)
+function get_detailled_radiative_data(path::String, selected_id::Integer)
     return get_detailled_crs_data(path, "CRs_details_radiative", mpi_ranks, selected_id)
 end
 
-function get_detailled_adiabatic_data(path::String, mpi_ranks::Int, selected_id::Int)
+function get_detailled_adiabatic_data(path::String, selected_id::Integer)
     return get_detailled_crs_data(path, "CRs_details_adiabatic", mpi_ranks, selected_id)
 end
